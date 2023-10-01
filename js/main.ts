d52cc84319dfc26ade78cf56058937a160ff61f4
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-10-02 01:02:47 +0200

    border (non funziona correttamente)

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 
+import Mesh from './mesh'
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
@@ -7,191 +9,22 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
-class Mesh {
-    buffer: ArrayBuffer
-    vertices: Float32Array
-    indices: number[]
-
-    constructor() {
-        this.buffer = new ArrayBuffer(0)
-        this.vertices = new Float32Array()
-        this.indices = []
-    }
-
-    resizeVertices(n) {
-        const bufferSize = this.buffer.byteLength
-        const requiredSize = 4*3*n
-        if (bufferSize < requiredSize) {     
-            const newBufferSize = Math.max(2*bufferSize,requiredSize)
-            console.log("reallocating", newBufferSize)
-            this.buffer = new ArrayBuffer(newBufferSize)
-            const newVertices = new Float32Array(this.buffer, 0, 3*n)
-            newVertices.set(this.vertices)
-            this.vertices = newVertices
-        } else {
-            this.vertices = new Float32Array(this.buffer, 0, 3*n)
-        }
-    }
-
-    addVertex(x, y, z) {
-        const n = this.vertices.length / 3
-        this.resizeVertices(n+1)
-        this.vertices[3*n] = x
-        this.vertices[3*n + 1] = y
-        this.vertices[3*n + 2] = z
-        return n
-    }
-
-    addTriangle(a, b, c) {
-        this.indices.push(a, b, c)
-    }
-
-    squaredDistanceBetweenVertices(a, b) {
-        const x1 = this.vertices[3*a]
-        const y1 = this.vertices[3*a + 1]
-        const z1 = this.vertices[3*a + 2]
-        const x2 = this.vertices[3*b]
-        const y2 = this.vertices[3*b + 1]
-        const z2 = this.vertices[3*b + 2]
-        return Math.sqrt( (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2 )
-    }
-
-    computeMaxEdgeLengthSquared() {
-        var maxEdgeLength = 0
-        const n = this.vertices.length / 3
-        for (var t=0; t<n ; t++) {
-            const a = this.indices[3*t]
-            const b = this.indices[3*t + 1]
-            const c = this.indices[3*t + 2]
-            maxEdgeLength = Math.max(maxEdgeLength, this.squaredDistanceBetweenVertices(a,b))
-            maxEdgeLength = Math.max(maxEdgeLength, this.squaredDistanceBetweenVertices(b,c))
-            maxEdgeLength = Math.max(maxEdgeLength, this.squaredDistanceBetweenVertices(c,a))
-        }
-        return maxEdgeLength
-    }
-
-    triangulate() {
-        const indices = this.indices
-        const nTriangles = indices.length / 3
-        const maxEdgeLengthSquared = this.computeMaxEdgeLengthSquared()
-
-        console.log(`old indices: ${indices}`)
-        this.printVertices()
-
-        let lastIndex = this.vertices.length / 3
-        const newVertices: {[key:string]:[number,number,number]} = {} 
-        const newIndices: number[] = []
-         
-        const key = (a,b) => {
-            if (a>b) return `${b}:${a}`
-            else return `${a}:${b}`
-        }
-
-        const split = (a, b) => {
-            const d = this.squaredDistanceBetweenVertices(a,b)
-            if (d < 0.5*maxEdgeLengthSquared) return
-            if (a>b) [a,b] = [b,a]
-            const key = `${a}:${b}`
-            if (key in newVertices) return
-            newVertices[key] = [a,b,lastIndex++]
-        }
-
-        for (let t=0 ; t<indices.length ; t+=3) {
-            split(indices[t], indices[t+1])
-            split(indices[t+1], indices[t+2])
-            split(indices[t+2], indices[t])
-        }
-
-        for (let t=0 ; t<indices.length ; t+=3) {
-            const [a,b,c] = [indices[t], indices[t+1], indices[t+2]]
-            const mid_ab = newVertices[key(a,b)]
-            const mid_bc = newVertices[key(b,c)]
-            const mid_ca = newVertices[key(c,a)]
-            if (mid_ab) {
-                const ab = mid_ab[2]
-                if (mid_bc) {
-                    const bc = mid_bc[2]
-                    if (mid_ca) {
-                        const ca = mid_ca[2]
-                        newIndices.push(ab, bc, ca)
-                        newIndices.push(a, ab, ca)
-                        newIndices.push(ab, b, bc)
-                        newIndices.push(ca, bc, c)
-                    } else {
-                        newIndices.push(a, ab, c)
-                        newIndices.push(ab, bc, c)
-                        newIndices.push(ab, b, bc)
-                    }
-                } else {
-                    if (mid_ca) {
-                        const ca = mid_ca[2]
-                        newIndices.push(c, ca, b)
-                        newIndices.push(ca, ab, b)
-                        newIndices.push(ca, a, ab)
-                    } else {
-                        newIndices.push(a, ab, c)
-                        newIndices.push(ab, b, c)
-                    }
-                }
-            } else {
-                if (mid_bc) {
-                    const bc = mid_bc[2]
-                    if (mid_ca) {
-                        const ca = mid_ca[2]
-                        newIndices.push(b, bc, a)
-                        newIndices.push(bc, ca, a)
-                        newIndices.push(bc, c, ca)
-                    } else {
-                        newIndices.push(a, b, bc)
-                        newIndices.push(a, bc, c)
-                    }
-                } else {
-                    if (mid_ca) {
-                        const ca = mid_ca[2]
-                        newIndices.push(a, b, ca)
-                        newIndices.push(b, c, ca)
-                    } else {
-                        newIndices.push(a, b, c)
-                    }
-                }
-            }
-        }
-        console.log(`newVertices: ${JSON.stringify(newVertices)}`)
-        console.log(`newIndices: ${newIndices}`)
-        const values: [number,number,number][] = Object.values(newVertices)
-        this.resizeVertices(this.vertices.length + values.length)
-        const vertices = this.vertices
-        const offset = this.vertices.length 
-        for(let i=0; i<values.length; ++i) {
-            let [a,b,c] = values[i]
-            for (let j=0 ; j<3; j++) {
-                vertices[c*3 + j] = 0.5*(vertices[3*a+j]+vertices[3*b+j])
-            }
-        }
-        this.indices=newIndices
-        this.printVertices()
-    }
-
-    printVertices() {
-        const vertices = this.vertices
-        for (let i=0; 3*i<vertices.length; i++) {
-            console.log(`${i}: (${vertices[3*i]},${vertices[3*i+1]},${vertices[3*i+2]})`)
-        }
-    }
-}
-
 const myMesh = new Mesh()
 
-myMesh.addVertex(-1.0, -1.0,  1.0) // v0
-myMesh.addVertex( 1.0, -1.0,  1.0) // v1
-myMesh.addVertex( 1.0,  1.0,  1.0) // v2
-myMesh.addVertex(-1.0,  1.0,  1.0) // v3
+myMesh.addBorder(
+    t => ([Math.cos(2*Math.PI*t), Math.sin(2*Math.PI*t), 0*Math.sin(t)]),
+    2*Math.PI)
+
+myMesh.addBorderVertex(0) // v0
+myMesh.addBorderVertex(0.25) // v1
+myMesh.addBorderVertex(0.5) // v2
+myMesh.addBorderVertex(0.75) // v3
 
 myMesh.addTriangle(0, 1, 2)
 myMesh.addTriangle(2, 3, 0)
 
-myMesh.triangulate()
-myMesh.triangulate()
+//myMesh.triangulate()
+//myMesh.triangulate()
 
 const vertices = new Float32Array( [
 	-1.0, -1.0,  1.0, // v0
